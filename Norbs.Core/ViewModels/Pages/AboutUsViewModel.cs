d93edc3ec734e9d3b71e48c0502e1b93dcda9223
vucaj <vucaj1998@outlook.com>
2022-10-15 17:41:20 +0200

    feature: about us generated files

---
+++ Norbs.Core/ViewModels/Pages/AboutUsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Norbs.Core.Contexts;
+using Norbs.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Norbs.Core.ViewModels.Pages
+{
+	public class AboutUsViewModel : PageViewModel
+	{
+		public AboutUsViewModel(IPageContext<AboutUs> context) : base(context)
+		{
+			Modules = context.Page.Modules.OfType<IINestedContent>();
+		}
+
+		public IEnumerable<IINestedContent> Modules { get; }
+	}
+}
