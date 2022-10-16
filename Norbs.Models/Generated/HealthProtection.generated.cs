372466ca564f256d900c615f30d2c2d1d564c143
Aleksandra Prpic <a.prpic@vega.local>
2022-10-16 13:36:47 +0200

    Add health protection page

--- Norbs.Models/Generated/HealthProtection.generated.cs
+++ Norbs.Models/Generated/HealthProtection.generated.cs
@@ -20,37 +20,37 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 namespace Norbs.Models.Generated
 {
-	/// <summary>About us</summary>
-	[PublishedModel("aboutUs")]
-	public partial class AboutUs : PublishedContentModel, IFooter, IHeader, IPage, IPageIntro, ISiteSettings
+	/// <summary>Health Protection</summary>
+	[PublishedModel("healthProtection")]
+	public partial class HealthProtection : PublishedContentModel, IFooter, IHeader, IPage, IPageIntro, ISiteSettings
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.6")]
-		public new const string ModelTypeAlias = "aboutUs";
+		public new const string ModelTypeAlias = "healthProtection";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.6")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.6")]
 		public new static IPublishedContentType GetModelContentType()
 			=> PublishedModelUtility.GetModelContentType(ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.6")]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<AboutUs, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<HealthProtection, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(), selector);
 #pragma warning restore 0109
 
 		// ctor
-		public AboutUs(IPublishedContent content)
+		public HealthProtection(IPublishedContent content)
 			: base(content)
 		{ }
 
 		// properties
 
 		///<summary>
-		/// Modules
+		/// Module
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.6")]
-		[ImplementPropertyType("modules")]
-		public IEnumerable<AboutUsModule> Modules => this.Value<IEnumerable<AboutUsModule>>("modules");
+		[ImplementPropertyType("module")]
+		public IEnumerable<HealthProtectionModule> Module => this.Value<IEnumerable<HealthProtectionModule>>("module");
 
 		///<summary>
 		/// Copyright Text: The site copyright text.
