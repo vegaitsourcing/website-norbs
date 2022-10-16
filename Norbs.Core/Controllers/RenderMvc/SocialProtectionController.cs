bed2cc5ec805a0c99208e304b9b07e9f0b7e6c04
Aleksandra Prpic <a.prpic@vega.local>
2022-10-16 16:36:49 +0200

    Save progress

---
+++ Norbs.Core/Controllers/RenderMvc/SocialProtectionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using Norbs.Core.ViewModels.Pages;
+using Norbs.Models.Generated;
+
+namespace Norbs.Core.Controllers.RenderMvc
+{
+    public class SocialProtectionController : BasePageController<SocialProtection>
+    {
+        public ActionResult Index(HealthProtection model)
+            => CurrentTemplate(new HealthProtectionViewModel(CreatePageContext(model)));
+    }
+}
