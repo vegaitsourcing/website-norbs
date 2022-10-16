0576c045010d5f4c3cebedafbad342a27037efb4
j-ivosevic-beeit <j.ivosevic@beeit.rs>
2022-10-16 16:35:17 +0200

    Articles page

---
+++ Norbs.Core/ViewModels/Pages/ArticlesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Norbs.Core.Contexts;
+using Norbs.Models.Generated;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Norbs.Core.ViewModels.Pages
+{
+	public class ArticlesViewModel : PageViewModel
+	{
+		public ArticlesViewModel(IPageContext<Articles> context) : base(context)
+		{
+		}
+	}
+}
