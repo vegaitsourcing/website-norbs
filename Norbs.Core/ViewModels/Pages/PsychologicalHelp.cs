843faa16f10fced905e4c29dec7c9a29d8c3fc80
Aleksandra Prpic <a.prpic@vega.local>
2022-10-16 18:08:41 +0200

    Psychological help

---
+++ Norbs.Core/ViewModels/Pages/PsychologicalHelp.cs
@@ -0,0 +1,21 @@
+﻿using Norbs.Core.Contexts;
+using Norbs.Models.Generated;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Norbs.Core.ViewModels.Pages
+{
+    public class PsychologicalHelpViewModel : PageViewModel
+    {
+        public PsychologicalHelpViewModel(IPageContext<PsychologicalHelp> context) : base(context)
+        {
+            Modules = context.Page.Module.OfType<IINestedContent>();
+        }
+
+        public IEnumerable<IINestedContent> Modules { get; }
+    }
+}
+
