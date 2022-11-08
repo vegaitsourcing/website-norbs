6add6438c131b574ebbfa0f7978e8efd71e31979
Igor Gajic <i.gajic@vegait.rs>
2022-11-08 04:46:24 -0800

    Implemented contact page

---
+++ Norbs.Core/Controllers/RenderMvc/ContactPageController.cs
@@ -0,0 +1,12 @@
+﻿using Norbs.Core.ViewModels.Pages;
+using Norbs.Models.Generated;
+using System.Web.Mvc;
+
+namespace Norbs.Core.Controllers.RenderMvc
+{
+    public class ContactPageController : BasePageController<ContactPage>
+    {
+        public ActionResult Index(ContactPage model)
+            => CurrentTemplate(new ContactPageViewModel(CreatePageContext(model)));
+    }
+}
---
+++ Norbs.Core/ViewModels/Pages/ContactPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Norbs.Core.Contexts;
+using Norbs.Models.Generated;
+
+namespace Norbs.Core.ViewModels.Pages
+{
+    public class ContactPageViewModel : PageViewModel
+    {
+        public ContactPageViewModel(IPageContext<ContactPage> context) : base(context)
+        {
+            this.Title = context.Page.PageTitle;
+            this.ContactDetails = context.Page.ContactDetails.ToHtmlString();
+        }
+
+        public string Title { get; set; }
+
+        public string ContactDetails { get; set; }
+    }
+}
