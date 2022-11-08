c9b51e5ddff860fcf2dfb546ae597b88130223ba
Igor Gajic <i.gajic@vegait.rs>
2022-11-07 23:03:30 -0800

    Implemented resource center page

---
+++ Norbs.Core/Models/VideoResource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using Norbs.Models.Extensions;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Norbs.Core.Models
+{
+    public class VideoResource : BaseResource
+    {
+        public VideoResource(IPublishedContent node)
+        {
+            Title = node.GetPropertyValue<string>("title");
+            VideoUrl = node.GetPropertyValue<string>("videoLink");
+        }
+
+        public string VideoUrl { get; set; }
+
+        public string EmbedUrl
+        {
+            get
+            {
+                try
+                {
+                    var url = new Uri(VideoUrl);
+                    var queryParams = HttpUtility.ParseQueryString(url.Query);
+                    var videoId = queryParams["v"];
+                    return $"https://www.youtube.com/embed/{videoId}";
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
